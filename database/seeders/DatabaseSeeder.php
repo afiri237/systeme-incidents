ec4a8635fa28aba5951212692451b40f2f7bd787
OTNIEL237 <feunoufossootniel123@gmail.com>
2026-05-18 18:50:08 +0200

    Add Seeder for Admin and data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,27 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Equipment;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // 1. Créer l'Admin
+        User::create([
+            'name' => 'Admin Pro',
+            'email' => 'admin@exemple.com', // <--- METS TON EMAIL ICI
+            'password' => Hash::make('password123'), // <--- METS TON MOT DE PASSE
+            'role' => 'admin',
         ]);
+
+        // 2. Créer les Catégories
+        Category::insert([['label' => 'Logiciel'], ['label' => 'Matériel'], ['label' => 'Réseau']]);
+
+        // 3. Créer les Équipements
+        Equipment::insert([['name' => 'PC Dell'], ['name' => 'Imprimante HP']]);
     }
 }
